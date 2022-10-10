60aead9bd94f85ca20697697be34df63daa9ca4c
Rajesh S <rajbca00@gmail.com>
2022-10-10 21:17:14 +0530

    package,module notes added

---
+++ Advanced Python/Notes/my_module.py
@@ -0,0 +1,11 @@
+data = {
+    "name":"ineuron",
+    'course':["data science","blockchain","drone","robotics","cloud"],
+    "greeting":"greeting from ineuron"
+}
+
+def get_course():
+    return data["course"]
+
+def greeting():
+    return data["greeting"]
---
+++ Advanced Python/Notes/test1.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    return a+b
+
+
+def sub(a,b):
+    return a-b
+
+
+def mul(a,b):
+    """
+    This is a function which can return a multiplication of 2 numbers
+    """
+    return a*b
+
+
+def div(a,b):
+    return a/b
